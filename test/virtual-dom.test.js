cdefc93fafd9956b90ee09e3918eceafb16c0a91
Aleksander Świtalski <aswitalski@opera.com>
2017-01-20 13:11:13 +0100

    Add more unit tests for virtual DOM

--- test/virtual-dom.test.js
+++ test/virtual-dom.test.js
@@ -431,7 +431,6 @@ describe('Virtual DOM', () => {
       assert(console.error.called);
     });
 
-
     it('rejects a text element with child nodes', () => {
 
       // given
@@ -466,6 +465,236 @@ describe('Virtual DOM', () => {
       assert(result.error instanceof Error);
       assert.equal(result.error.message, 'Text elements cannot have child nodes');
       assert(console.error.called);
+    });
+
+    it('rejects a subcomponent with text content', () => {
+
+      // given
+      const component = Symbol.for('component');
+      const template = [
+        component, 'Text'
+      ];
+
+      // when
+      const result = VirtualDOM.validate(template);
+
+      // then
+      assert(result.error instanceof Error);
+      assert.equal(result.error.message, 'Subcomponents do not accept text content');
+      assert(console.error.called);
+    });
+
+    it('rejects a subcomponent with properties and text content', () => {
+
+      // given
+      const component = Symbol.for('component');
+      const props = {
+        prop: 'prop'
+      };
+      const template = [
+        component, props, 'Text'
+      ];
+
+      // when
+      const result = VirtualDOM.validate(template);
+
+      // then
+      assert(result.error instanceof Error);
+      assert.equal(result.error.message, 'Subcomponents do not accept text content');
+      assert(console.error.called);
+    });
+  });
+
+  describe('=> spread template', () => {
+
+    it('detects component', () => {
+
+      // given
+      const component = Symbol.for('component');
+      const template = [
+        component
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        component
+      });
+    });
+
+    it('detects component with properties', () => {
+
+      // given
+      const component = Symbol.for('component');
+      const props = {
+        prop: 'prop'
+      };
+      const template = [
+        component, props
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        component,
+        props
+      });
+    });
+
+    it('detects component with properties and child nodes', () => {
+
+      // given
+      const component = Symbol.for('component');
+      const props = {
+        prop: 'prop'
+      };
+      const children = [
+        ['div'],
+        ['span']
+      ];
+      const template = [
+        component, props, ...children
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        component,
+        props,
+        children
+      });
+    });
+
+    it('detects empty element', () => {
+
+      // given
+      const template = [
+        'div'
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name: 'div'
+      });
+    });
+
+    it('detects empty element with properties', () => {
+
+      // given
+      const props = {
+        prop: 'prop'
+      };
+      const template = [
+        'div', props
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name: 'div',
+        props
+      });
+    });
+
+    it('detects text element', () => {
+
+      // given
+      const name = 'div';
+      const text = 'text';
+      const template = [
+        name, text
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name,
+        text
+      });
+    });
+
+    it('detects text element with properties', () => {
+
+      // given
+      const name = 'div';
+      const text = 'text';
+      const props = {
+        prop: 'prop'
+      };
+      const template = [
+        name, props, text
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name,
+        props,
+        text
+      });
+    });
+
+    it('detects element with child nodes', () => {
+
+      // given
+      const name = 'div';
+      const children = [
+        ['div'],
+        ['span'],
+      ];
+      const template = [
+        name, ...children
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name,
+        children
+      });
+    });
+
+    it('detects element with properties and child nodes', () => {
+
+      // given
+      const name = 'div';
+      const props = {
+        prop: 'prop'
+      };
+      const children = [
+        ['div'],
+        ['span'],
+      ];
+      const template = [
+        name, props, ...children
+      ];
+
+      // when
+      const definition = VirtualDOM.spread(template);
+
+      // then
+      assert.deepEqual(definition, {
+        name,
+        props,
+        children
+      });
     });
   });
 
