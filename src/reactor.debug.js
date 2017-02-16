ae7a079bab1d2661f0b06da515e5afcdd9daf769
Aleksander Świtalski <aswitalski@opera.com>
2017-02-16 17:19:11 +0100

    Ensure proper initialisation

--- src/reactor.debug.js
+++ src/reactor.debug.js
@@ -1,16 +1,27 @@
 (async () => {
 
   require.prefix('core', 'src/');
+
   let initialized = false;
+  let readyPromise = Promise.resolve();
+
+  const init = async () => {
+    if (initialized) {
+      return;
+    }
+    const reactor = await require('core/reactor');
+    const modules = await reactor.init();
+    Object.assign(Reactor, modules);
+    Object.freeze(Reactor);
+    initialized = true;
+  };
 
   window.Reactor = {
     ready: async () => {
       if (!initialized) {
-        const reactor = await require('core/reactor');
-        const modules = await reactor.init();
-        Object.assign(Reactor, modules);
-        initialized = true;
+        readyPromise = readyPromise.then(() => init());
       }
+      await readyPromise;
     }
   };
 
