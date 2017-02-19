068a53b3eea544928ba690e8a8e04ebf65a38a10
aswitalski <aleksander.switalski@gmail.com>
2017-02-19 15:50:14 +0100

    Create unit tests for Document

--- test/config/init.js
+++ test/config/init.js
@@ -5,6 +5,44 @@ global.sinon = require('sinon');
 const consts = require('../../src/core/consts.js');
 const utils = require('../../src/core/consts.js');
 
+global.Element = class {
+
+  constructor(name) {
+    this.tagName = name.toUpperCase();
+    this.childNodes = [];
+    this.eventListeners = {};
+  }
+
+  setAttribute(name, value) {
+    throw new Error('Function "setAttribute" not implemented!');
+  }
+
+  removeAttribute(name) {
+    throw new Error('Function "removeAttribute" not implemented!');
+  }
+
+  appendChild(child) {
+    this.childNodes.push(child);
+  }
+
+  insertBefore(child) {
+    throw new Error('Function "insertBefore" not implemented!');
+  }
+
+  addEventListener(name, listener) {
+    this.eventListeners[name] = this.eventListeners[name] || [];
+    this.eventListeners[name].push(listener);
+  }
+
+  removeEventListener(name, listener) {
+    throw new Error('Function "removeEventListener" not implemented!');
+  }
+};
+
+global.document = {
+  createElement: name => new Element(name)
+};
+
 global.createCore = () => ({
   Component: require('../../src/core/component.js'),
   VirtualNode: require('../../src/core/virtual-node.js'),
@@ -13,6 +51,7 @@ global.createCore = () => ({
   Diff: require('../../src/core/diff.js'),
   Patch: require('../../src/core/patch.js'),
   Reconciler: require('../../src/core/reconciler.js'),
+  Document: require('../../src/core/document.js'),
   SUPPORTED_ATTRIBUTES: consts.SUPPORTED_ATTRIBUTES,
   SUPPORTED_EVENTS: consts.SUPPORTED_EVENTS,
   create: utils.create
---
+++ test/document.test.js
@@ -0,0 +1,270 @@
+global.Reactor = createCore();
+const VirtualNode = Reactor.VirtualNode;
+const ComponentTree = Reactor.ComponentTree;
+const Document = Reactor.Document;
+
+describe('Document', () => {
+
+  const Component = Symbol.for('Component');
+  const Subcomponent = Symbol.for('Subcomponent');
+
+  const ComponentClass = class extends Reactor.Component {
+    render() {
+      return this.children[0] || null;
+    }
+  };
+  const SubcomponentClass = class extends Reactor.Component {
+    render() {
+      return this.children[0] || null;
+    }
+  };
+
+  const createDummyInstance = def => {
+    switch (def) {
+      case Component:
+        return new ComponentClass();
+      case Subcomponent:
+        return new SubcomponentClass();
+    }
+  };
+
+  describe('=> create element', () => {
+
+    it('supports empty elements', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'span'
+      ]);
+
+      // when
+      const element = Document.createElement(node);
+
+      // then
+      assert(element instanceof Element);
+      assert.equal(element.tagName, 'SPAN')
+      assert.equal(element.textContent, undefined);
+    });
+
+    it('supports text elements', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'span', 'Text'
+      ]);
+
+      // when
+      const element = Document.createElement(node);
+
+      // then
+      assert(element instanceof Element);
+      assert.equal(element.tagName, 'SPAN')
+      assert.equal(element.textContent, 'Text');
+    });
+
+    it('supports adding event listeners', () => {
+
+      // given
+      const onClick = () => {};
+      const onChange = () => {};
+      const node = ComponentTree.createFromTemplate([
+        'span', {
+          onClick,
+          onChange
+        }, 'Text'
+      ]);
+
+      // when
+      const element = Document.createElement(node);
+
+      // then
+      assert(element instanceof Element);
+      assert.equal(element.tagName, 'SPAN')
+      assert.equal(element.textContent, 'Text');
+      assert.deepEqual(element.eventListeners, {
+        click: [onClick],
+        change: [onChange],
+      });
+    });
+  });
+
+  describe('=> create bound tree', () => {
+
+    beforeEach(() => {
+      ComponentTree.createInstance = createDummyInstance;
+    });
+
+    it('creates a single element', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div'
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+    });
+
+    it('creates two nested elements', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div', [
+          'span'
+        ]
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+
+      const spanNode = node.children[0];
+      const spanElement = element.childNodes[0];
+      assert.equal(spanNode.name, 'span');
+      assert.equal(spanNode.ref, spanElement);
+      assert.equal(spanElement.tagName, 'SPAN');
+    });
+
+    it('creates three nested elements', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div', [
+          'span', [
+            'a'
+          ]
+        ]
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+
+      const spanNode = node.children[0];
+      const spanElement = element.childNodes[0];
+      assert.equal(spanNode.name, 'span');
+      assert.equal(spanNode.ref, spanElement);
+      assert.equal(spanElement.tagName, 'SPAN');
+
+      const linkNode = spanNode.children[0];
+      const linkElement = spanElement.childNodes[0];
+      assert.equal(linkNode.name, 'a');
+      assert.equal(linkNode.ref, linkElement);
+      assert.equal(linkElement.tagName, 'A');
+    });
+
+    it('supports component present within the tree', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div', [
+          Component, [
+            'span'
+          ]
+        ]
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+
+      const componentNode = node.children[0];
+      assert.equal(componentNode.constructor, ComponentClass);
+
+      const spanNode = componentNode.child;
+      const spanElement = element.childNodes[0];
+      assert.equal(spanNode.name, 'span');
+      assert.equal(spanNode.ref, spanElement);
+      assert.equal(spanElement.tagName, 'SPAN');
+    });
+
+    it('supports nested components present within the tree', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div', [
+          Component, [
+            Subcomponent, [
+              'span'
+            ]
+          ]
+        ]
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+
+      const componentNode = node.children[0];
+      assert.equal(componentNode.constructor, ComponentClass);
+      assert(componentNode instanceof Reactor.Component);
+
+      const subcomponentNode = componentNode.child;
+      assert.equal(subcomponentNode.constructor, SubcomponentClass);
+
+      const spanNode = subcomponentNode.child;
+      const spanElement = element.childNodes[0];
+      assert.equal(spanNode.name, 'span');
+      assert.equal(spanNode.ref, spanElement);
+      assert.equal(spanElement.tagName, 'SPAN');
+    });
+
+    it('supports component with no children', () => {
+
+      // given
+      const node = ComponentTree.createFromTemplate([
+        'div', [
+          Component, [
+            'span', [
+              Subcomponent
+            ]
+          ]
+        ]
+      ]);
+
+      // when
+      const element = Document.createBoundTree(node);
+
+      // then
+      assert.equal(node.name, 'div');
+      assert.equal(node.ref, element);
+      assert.equal(element.tagName, 'DIV');
+
+      const componentNode = node.children[0];
+      assert.equal(componentNode.constructor, ComponentClass);
+      assert(componentNode instanceof Reactor.Component);
+
+      const spanNode = componentNode.child;
+      const spanElement = element.childNodes[0];
+      assert.equal(spanNode.name, 'span');
+      assert.equal(spanNode.ref, spanElement);
+      assert.equal(spanElement.tagName, 'SPAN');
+
+      const subcomponentNode = spanNode.children[0];
+      assert.equal(subcomponentNode.constructor, SubcomponentClass);
+
+      assert.equal(subcomponentNode.child, undefined);
+    });
+  });
+});
