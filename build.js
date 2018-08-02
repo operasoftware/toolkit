89e4bcabb20ec845a85253830b06bf6aec4ca21c
Aleksander Świtalski <aswitalski@opera.com>
2018-08-02 10:04:16 +0200

    Add copyright headers

--- build.js
+++ build.js
@@ -21,6 +21,24 @@ const normalizeModule = path => {
   return convertModuleExportsToDefine(script, path);
 };
 
+const copyrightHeader = `/*
+Copyright 2017-2018 Opera Software AS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+`;
+
 const merge = (...contents) => contents.join('\n\n');
 
 const package = loadJSON('./package.json');
@@ -45,11 +63,16 @@ const utils = normalizeModule('core/utils');
 const Toolkit = normalizeModule('core/toolkit');
 const Release = loadModule('release');
 
-const release = merge(
+let release = merge(
                     ModuleLoader, Consts, Nodes, Diff, Lifecycle, Patch,
                     Plugins, Reconciler, Renderer, Sandbox, Service, Template,
                     VirtualDOM, utils, Toolkit, Release)
                     .replace(/\n\n\n/g, '\n\n');
+
+while (release.includes(copyrightHeader)) {
+  release = release.replace(copyrightHeader, '');
+}
+release = `${copyrightHeader}${release}`;
 
 fs.writeFileSync('./dist/toolkit.release.js', release, 'utf8');
 
