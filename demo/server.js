f5945f29e2a3660ca506925eba82eae68af1430f
Aleksander Świtalski <aswitalski@opera.com>
2018-09-23 13:05:54 +0200

    Fix demo and web tests

--- demo/server.js
+++ demo/server.js
@@ -11,6 +11,7 @@ app.use('/', express.static('demo'));
 app.use('/loader', express.static('node_modules/lazy-module-loader'));
 app.use('/src', express.static('src'));
 app.use('/core', express.static('src/core'));
+app.use('/plugins', express.static('src/plugins'));
 app.use('/demo', express.static('demo/src'));
 app.use('/images', express.static('demo/images'));
 app.use('/styles', express.static('demo/styles'));
--- test/server.js
+++ test/server.js
@@ -8,8 +8,21 @@ const PORT = 3033;
 app.set('port', PORT);
 
 app.use('/', express.static('test'));
+app.use('/config', express.static('test/config'));
 app.use('/src', express.static('src'));
+app.use('/core', express.static('src/core'));
 app.use('/loader', express.static('node_modules/lazy-module-loader'));
+
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  response.setHeader('Pragma', 'no-cache');
+  response.setHeader('Expires', '0');
+  if (request.url.endsWith('.js')) {
+    response.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+  }
+  return next();
+});
 
 const server = app.listen(app.get('port'), () => {
   console.log('---------------------------------------------------');
