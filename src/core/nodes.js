6c77dfd07b741f493ee8e13d45e32744cfdd3f00
Aleksander Świtalski <aleksander.switalski@gmail.com>
2019-04-29 00:19:53 +0200

    Revert "Create children after installing plugins"
    This reverts commit 9a0876ba736e31e168684637e43a642534eaf15c.

--- src/core/nodes.js
+++ src/core/nodes.js
@@ -282,6 +282,9 @@ limitations under the License.
     constructor(description, parentNode = null) {
       super(description, parentNode, /*= attachDOM */ false);
       this.plugins = this.createPlugins();
+      if (description.children) {
+        this.createChildren();
+      }
       this.subroots = new Set();
       this.state = opr.Toolkit.Reducers.create(this);
       this.commands = new opr.Toolkit.Dispatcher(this);
@@ -307,11 +310,6 @@ limitations under the License.
 
       await this.plugins.installAll();
       opr.Toolkit.track(this);
-
-      if (this.description.children) {
-        this.createChildren();
-        this.attachChildren();
-      }
 
       const state = await this.getInitialState.call(
           this.sandbox, this.description.props || {});
@@ -426,6 +424,7 @@ limitations under the License.
     attachDOM() {
       if (this.constructor.elementName) {
         this.ref = this.createCustomElement();
+        this.attachChildren();
       } else {
         super.attachDOM();
       }
